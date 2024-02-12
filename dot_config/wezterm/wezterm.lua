ae63162d4e250204a8a66332e93818d603481f94
Akagi201 <akagi201@gmail.com>
2024-02-12 23:10:12 +0800

    feat: update wezterm configs

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,18 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require('wezterm')
 
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Dracula'
-config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font = wezterm.font('FiraCode Nerd Font Mono')
 config.font_size = 16
 config.line_height = 1.2
 config.command_palette_font_size = 16
-
-config.window_frame = {
-  font_size = 16,
-  font = wezterm.font("FiraCode Nerd Font Mono"),
-}
-
+config.hide_tab_bar_if_only_one_tab = true
 config.pane_focus_follows_mouse = true
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 10
 
 return config
